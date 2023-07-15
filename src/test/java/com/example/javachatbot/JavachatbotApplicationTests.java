4d71b4caa2217b3e0bbb825b2a433d5470ba6668
JacinthEsther <agbonirojacinta@gmail.com>
2023-07-15 16:03:42 +0100

    first commit

---
+++ src/test/java/com/example/javachatbot/JavachatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javachatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavachatbotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
